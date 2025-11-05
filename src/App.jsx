812d87a17293692b4f4e04ccdce9414eed8e915d
Cioban Fabian-Remus <fabian.cioban2002@gmail.com>
2025-11-05 13:19:00 +0200

    new project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+function App() {
+    const [mesaj, setMesaj] = useState("");
+
+    const getMesaj = async () => {
+        const res = await fetch("/api/hello");
+        const data = await res.json();
+        setMesaj(data.message);
+    };
+
+    return (
+        <div style={{textAlign: "center", marginTop: "100px"}}>
+            <h1>FabiHelp 🚀</h1>
+            <button onClick={getMesaj}>Primește mesaj de la server</button>
+            <p>{mesaj}</p>
+        </div>
+    );
+}
+
+export default App;
